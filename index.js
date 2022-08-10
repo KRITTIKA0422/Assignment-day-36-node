b8f60aa582ff5b390b987ac7d209d6821443c64a
Guvi-Assignment-day-21 <krittika489podder@gmail.com>
2022-08-10 13:41:49 +0530

    cors added

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import { moviesRouter } from "./routes/movies.js";
+import cors from "cors";
 dotenv.config();
 console.log(process.env.MONGO_URL);
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 const MONGO_URL = process.env.MONGO_URL;
+app.use(cors());
 async function createConnection() {
     const client = new MongoClient(MONGO_URL);
     await client.connect();
